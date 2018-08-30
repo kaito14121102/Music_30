5b9a40777e2250a7142ff275bd13d072583a8d47
kaito14121102 <luutuanminh.cntt@gmail.com>
2018-08-30 11:48:42 +0700

    repair file

--- app/src/main/java/com/framgia/music_30/screen/home/HomeActivity.java
+++ app/src/main/java/com/framgia/music_30/screen/home/HomeActivity.java
@@ -1,7 +1,9 @@
-package com.framgia.music_30;
+package com.framgia.music_30.screen.home;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.framgia.music_30.R;
 
 public class MainActivity extends AppCompatActivity {
 
